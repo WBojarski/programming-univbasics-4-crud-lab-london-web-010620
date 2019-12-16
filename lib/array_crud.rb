362a76cd7ddba9f5ff90530ce60fbaeeef742ad7
Wojciech Bojarski <bojawojt@gmail.com>
2019-12-16 20:37:52 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -1,3 +1,4 @@
+
 def create_an_empty_array
 	[]
 end
@@ -7,9 +8,8 @@ def create_an_array
 end
 
 def add_element_to_end_of_array(array, element)
-  
+  array << element
 end
-
 def add_element_to_start_of_array(array, element)
   
 end
